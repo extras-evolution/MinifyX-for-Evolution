808071118ae549c5872b407383c36a088bf07bd6
Dmi3yy <dmi3yy@gmail.com>
2014-10-16 12:08:36 +0300

    сыы first version ready

--- assets/snippets/cssjs/snippet.css.php
+++ assets/snippets/cssjs/snippet.css.php
@@ -3,44 +3,39 @@ if(!defined('MODX_BASE_PATH')) {die('What are you doing? Get out of here!');}
 
 //параметры
 $files = isset($files) ? $files : ''; // Список файлов (css, scss, less)
-$inline = isset($inline) ? $inline : ''; // инлайн код стилей
 $minify = isset($minify) ? $minify : '1'; //сжымать и минифицировать файлы
-$folder = isset($folder) ? $folder : 'assets/templates/'; // папка для сгенерированных стилей по умолчанию
-$parse = isset($parse) ? $parse : '0'; //обрабатывать ли теги MODX
-
-
+$folder = isset($folder) ? $folder : ''; // папка для сгенерированных стилей по умолчанию в корень
+//$inline = isset($inline) ? $inline : ''; // инлайн код стилей
+//$parse = isset($parse) ? $parse : '0'; //обрабатывать ли теги MODX
 
 //Обрабатываем файлы, преобразовываем less и scss
-$files = explode(',', str_replace('\n', '', $files));
-foreach ($files as $key => $value) {
+$filesArr = explode(',', str_replace('\n', '', $files));
+foreach ($filesArr as $key => $value) {
 	$file = MODX_BASE_PATH . trim($value);
 	$fileinfo = pathinfo($file);
+	$v[$key] =  filemtime($file);
 	switch ($fileinfo['extension']) {
 		case 'css':
-		$files[$key] = $file;     
+		$filesForMin[$key] = $file;  
 		break;
-		case 'less':
+		/*case 'less':
 		require_once(MODX_BASE_PATH. "assets/snippets/cssjs/less.inc.php"); 
 		$less = new lessc;
-		$less->checkedCompile($file, $folder.$fileinfo.'.css');
-		$files[$key] = $folder.$fileinfo.'.css';
-		break;
-		case 'scss':
-		require_once(MODX_BASE_PATH. "assets/snippets/cssjs/scss.inc.php"); 
-		$scss = new scssc();
-		$scss->checkedCompile($file, $folder.$fileinfo.'.css');
-		$files[$key] = $folder.$fileinfo.'.css';
-		break;
+		$less->checkedCompile($file, $folder.$fileinfo['filename'].'.css');
+		$filesForMin[$key] = $folder.$fileinfo['filename'].'.css';
+		break;*/
 	}
-
 }
-//Обрабатываем инлайн тут думаю логично просто сохранять в файлик а после обрабатывать как все остальные но надо думать 
-//Минифицируем 
 if ($minify == '1') {
 	include_once(MODX_BASE_PATH. "assets/snippets/cssjs/class.magic-min.php"); 
 	$minified = new Minifier();
-	$min = $minified->merge( MODX_BASE_PATH.$folder.'styles.min.css', 'css', $files, '1.1' );
+	$min = $minified->merge( MODX_BASE_PATH.$folder.'styles.min.css', 'css', $filesForMin );
+	return '<link rel="stylesheet" href="/'.$folder.'styles.min.css?v='.substr(md5(max($v)),0,3).'" />';
+}else{
+	$links = '';
+	foreach ($filesArr as $key => $value) {
+		$links .= '<link rel="stylesheet" href="/'.trim($value).'?v='.substr(md5($v[$key]),0,3).'" />';	
+	}	
+	return $links;
 }
-//отдаем на сайт
-return '<link rel="stylesheet" href="/'.$folder.'styles.min.css" />';
 ?>
